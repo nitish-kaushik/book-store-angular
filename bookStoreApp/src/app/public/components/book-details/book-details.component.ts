fb16e5648a3cfc358f9f2acd0abd0d8ee2d7eed7
Nitish <nitish.webgentle@gmail.com>
2021-08-26 12:18:27 +0530

    get the param value using ActivatedRoute interface

--- bookStoreApp/src/app/public/components/book-details/book-details.component.ts
+++ bookStoreApp/src/app/public/components/book-details/book-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.scss']
+  styleUrls: ['./book-details.component.scss'],
 })
 export class BookDetailsComponent implements OnInit {
+  public id: number = 0;
+  public authorId: number = 0;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((param) => {
+      this.id = param.id;
+      this.authorId = param['authorId'];
+    });
   }
-
 }
