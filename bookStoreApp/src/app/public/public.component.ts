cd6e7597d32396afd3f204f98f6d58aa50a8b19d
Nitish <nitish.webgentle@gmail.com>
2021-08-26 12:32:04 +0530

    get the param value from parent and child route

--- bookStoreApp/src/app/public/public.component.ts
+++ bookStoreApp/src/app/public/public.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-public',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class PublicComponent implements OnInit {
   bookId: number = 1;
   authorId: number = 100;
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.firstChild?.params.subscribe((param) => {
+      console.log(param);
+    });
+  }
 }
--- bookStoreApp/src/app/user/components/delete-book/delete-book.component.ts
+++ bookStoreApp/src/app/user/components/delete-book/delete-book.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-delete-book',
   templateUrl: './delete-book.component.html',
-  styleUrls: ['./delete-book.component.scss']
+  styleUrls: ['./delete-book.component.scss'],
 })
 export class DeleteBookComponent implements OnInit {
+  userId: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.parent?.params.subscribe((param) => {
+      console.log(param);
+      this.userId = param.userId;
+    });
   }
-
 }
