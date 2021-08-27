63d566cf30f7ed76a6e19be4e781bf898a620e97
Nitish <nitish.webgentle@gmail.com>
2021-08-27 10:16:03 +0530

    added angular material

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, AboutUsComponent, HowItWorksComponent],
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
     // PublicModule,
     SharedModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
