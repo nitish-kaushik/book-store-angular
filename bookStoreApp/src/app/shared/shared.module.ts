ebdff1e9f2a597e5f7e6f96ca05470e4669d78bc
Nitish <nitish.webgentle@gmail.com>
2022-05-08 11:30:55 +0530

    create a separate component for toolbar

--- bookStoreApp/src/app/shared/shared.module.ts
+++ bookStoreApp/src/app/shared/shared.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RouterModule } from '@angular/router';
-
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [NotFoundComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [NotFoundComponent, ToolbarComponent],
+  imports: [CommonModule, RouterModule, MatToolbarModule,
+    MatIconModule,
+    MatButtonModule],
+  exports: [ToolbarComponent]
 })
-export class SharedModule {}
+export class SharedModule { }
