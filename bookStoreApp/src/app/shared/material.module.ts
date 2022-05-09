58e502e1c81473c83397303fb2ccfe740431f257
Nitish <nitish.webgentle@gmail.com>
2022-05-10 00:24:31 +0530

    created a separate module for angular material components

---
+++ bookStoreApp/src/app/shared/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ]
+})
+export class MaterialModule { }
--- bookStoreApp/src/app/shared/shared.module.ts
+++ bookStoreApp/src/app/shared/shared.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+
+import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [NotFoundComponent, ToolbarComponent],
-  imports: [CommonModule, RouterModule, MatToolbarModule,
-    MatIconModule,
-    MatButtonModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   exports: [ToolbarComponent]
 })
 export class SharedModule { }
