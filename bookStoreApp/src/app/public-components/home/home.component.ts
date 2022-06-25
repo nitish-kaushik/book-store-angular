42d0795df05b640267d2a602594eed290e88fac5
Nitish <nitish.webgentle@gmail.com>
2022-06-25 23:12:38 +0530

    @ViewChild in angular

--- bookStoreApp/src/app/public-components/home/home.component.ts
+++ bookStoreApp/src/app/public-components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
 import { CounterService } from 'src/app/shared/services/counter.service';
 
 @Component({
@@ -6,13 +8,27 @@ import { CounterService } from 'src/app/shared/services/counter.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('authors') private childAuthor: AuthorsComponent;
+  @ViewChild('decButton') private decButton: MatButton;
+  @ViewChild('title') private title: ElementRef;
 
   constructor(public _counterService: CounterService,
     @Inject('appTitle') public config: any
   ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.childAuthor.setData(10);
+      this.decButton.color = 'primary'
+      this.decButton.disabled = true;
+      this.title.nativeElement.innerHTML = 'This is updated title';
+      console.log(this.title);
+    }, 0);
   }
 
   public increase(): void {
