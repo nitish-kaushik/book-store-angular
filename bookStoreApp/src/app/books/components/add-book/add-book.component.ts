c7d7aa439ce32fde33877fba0b4061ec1d9f95e7
Nitish <nitish.webgentle@gmail.com>
2022-07-22 21:33:27 +0530

    2way binding in angular

--- bookStoreApp/src/app/books/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/books/components/add-book/add-book.component.ts
@@ -9,24 +9,38 @@ import { BookService } from '../../services/book.service';
 })
 export class AddBookComponent implements OnInit {
 
+  prices: any[] = [
+    { value: 100, viewValue: '$ 100' },
+    { value: 200, viewValue: '$ 200' },
+    { value: 300, viewValue: '$ 300' },
+  ];
+  public model: BookModel;
   constructor(private _bookService: BookService) { }
 
   ngOnInit(): void {
+    this.model = new BookModel();
+    this.model.title = 'Book';
+    this.model.totalPages = 100;
+    this.model.price = {
+      value: 100,
+      currency: 'USD'
+    }
+    this.model.isPublished = true;
   }
 
 
-  saveBook(value: any): void {
-    console.log(value);
-    const book: BookModel = new BookModel();
-    book.title = value.title;
-    book.author = value.author;
-    book.totalPages = value.pages;
-    book.price = {
-      currency: "$",
-      value: value.price
-    };
-    book.isPublished = value.isPublished;
-    book.publishedOn = value.publishedOn;
-    this._bookService.addBook(book);
+  saveBook(): void {
+    console.log(this.model);
+    // const book: BookModel = new BookModel();
+    // book.title = value.title;
+    // book.author = value.author;
+    // book.totalPages = value.pages;
+    // book.price = {
+    //   currency: "$",
+    //   value: value.price
+    // };
+    // book.isPublished = value.isPublished;
+    // book.publishedOn = value.publishedOn;
+    this._bookService.addBook(this.model);
   }
 }
--- bookStoreApp/src/app/books/models/book.model.ts
+++ bookStoreApp/src/app/books/models/book.model.ts
@@ -4,18 +4,8 @@ export class BookModel {
     public id: number;
     public title: string;
     public totalPages: number;
-    private _author: string;
+    public author: string;
     public price: PriceModel
     public isPublished: boolean;
     public publishedOn: Date;
-
-    public set author(value: string) {
-        this._author = 'Author: ' + value;
-    }
-
-    public get author() {
-        return this._author;
-    }
-
-
 }
