c9c5d1be71abd438029fe49bc10065768c06a352
Nitish <nitish.webgentle@gmail.com>
2022-07-25 22:02:42 +0530

    Added httpClient

--- bookStoreApp/src/app/shared/shared.module.ts
+++ bookStoreApp/src/app/shared/shared.module.ts
@@ -8,9 +8,10 @@ import { MaterialModule } from './material.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { AuthorsComponent } from './components/authors/authors.component';
 import { AuthorsAddressComponent } from './components/authors-address/authors-address.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [NotFoundComponent, ToolbarComponent, FooterComponent, AuthorsComponent, AuthorsAddressComponent],
-  imports: [CommonModule, RouterModule, MaterialModule],
-  exports: [ToolbarComponent, FooterComponent, MaterialModule, AuthorsComponent, AuthorsAddressComponent]
+  imports: [CommonModule, RouterModule, MaterialModule, HttpClientModule],
+  exports: [ToolbarComponent, FooterComponent, MaterialModule, AuthorsComponent, AuthorsAddressComponent, HttpClientModule]
 })
 export class SharedModule { }
